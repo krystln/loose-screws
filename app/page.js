f30fe05899c98f59c52a2ad49546db68c5ab2844
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2023-05-30 19:18:36 +0530

    build safe version

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 const Home = () => {
 	return (
 		<>
-			<div className="head_text">We don't <span className="orange_gradient">Screw</span> with you</div>
+			<div className="head_text">We don&apos;t <span className="orange_gradient">Screw</span> with you</div>
 			<div className="desc text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, deserunt, cumque sed modi tempore sint incidunt libero qui possimus autem quam quas accusantium eius deleniti magnam at perferendis iure quia.</div>
 		</>
 	)
