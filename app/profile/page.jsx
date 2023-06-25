92f2359c678051710f0ccbe9ccef68091120ba7c
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2023-06-25 18:49:03 +0530

    Profile pg modified

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -6,26 +6,36 @@ const Profile = () => {
 
     const { data: session } = useSession();
 
-  return (
-    <>
-    <div className="flex">
-        <Image
-            src={session?.user.image}
-            width={200}
-            height={200}
-            className='object-contain rounded-full'
-            />
-        <div>
-        <h1 className="head_text">{session?.user.name}</h1>
-        <p className="desc">{session?.user.email}</p>
-        </div>
-    </div>
+    const CartItems = session?.user.cart.map((item) => {
+        return <div className="flex">{item.name}</div>
+    })
 
-    <div className="border-2 border-gray-400 rounded-md w-4/5 h-52">
-        <div className="mt-5 text-lg text-black max-w-fit sm:text-xl bg-white relative -top-9 ml-4 px-3  ">Your Cart</div>
-    </div>
-            </>
-  )
+    return (
+        <>
+            <div className="flex">
+                <Image
+                    src={session?.user.image}
+                    width={200}
+                    height={200}
+                    className='object-contain rounded-full'
+                />
+                <div>
+                    <h1 className="head_text">{session?.user.name}</h1>
+                    <p className="desc">{session?.user.email}</p>
+                </div>
+            </div>
+
+            <div className="border-2 border-gray-400 rounded-md w-4/5 h-52">
+                <div className="mt-5 text-lg text-black max-w-fit sm:text-xl bg-white relative -top-9 ml-4 px-3  ">Your Cart</div>
+                {CartItems}
+                {(CartItems.count() === 0) ? <div>Explore the Store</div> : <div>Buy Items</div>}
+            </div>
+
+
+
+
+        </>
+    )
 }
 
 export default Profile;
